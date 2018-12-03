cc85a1cf674317f7c44b6b8a6a9e7c4086d7b835
Sami Turcotte <samiturcotte@gmail.com>
2018-12-03 00:59:31 -0500

    Fix reduce sample

--- samples/reduce.js
+++ samples/reduce.js
@@ -2,4 +2,5 @@ const Mhysa = require("mhysa");
 
 Mhysa.fromArray(["a", "b", "cc"])
     .pipe(Mhysa.reduce((acc, s) => ({ ...acc, [s]: s.length }), {}))
+    .pipe(Mhysa.stringify())
     .pipe(process.stdout);
