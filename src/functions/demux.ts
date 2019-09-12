426f861ce3bce0edfb7e336857e43f6a3bbd887b
Jerry Kurian <jerrykurian@protonmail.com>
2019-09-12 15:35:59 -0400

    Cleanup console logs

--- src/functions/demux.ts
+++ src/functions/demux.ts
@@ -58,21 +58,7 @@ class Demux extends Writable {
             this.streamsByKey[destKey] = this.construct(destKey);
         }
         if (!this.streamsByKey[destKey].write(chunk, encoding)) {
-            console.log(
-                "waiting drain",
-                chunk,
-                this._writableState.length,
-                this.streamsByKey[destKey]._writableState.length,
-                this.streamsByKey[destKey]._readableState.length,
-            );
             this.streamsByKey[destKey].once("drain", () => {
-                console.log(
-                    "calling cb after drain",
-                    chunk,
-                    this._writableState.length,
-                    this.streamsByKey[destKey]._writableState.length,
-                    this.streamsByKey[destKey]._readableState.length,
-                );
                 cb();
             });
         } else {
