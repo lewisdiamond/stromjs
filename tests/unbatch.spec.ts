df2a660fe90af89f5373764b77318a1051a5ad73
Lewis Diamond <git@lewisdiamond.com>
2019-12-02 16:14:32 -0500

    Removed useless params in unbatch

--- tests/unbatch.spec.ts
+++ tests/unbatch.spec.ts
@@ -10,7 +10,7 @@ test.cb("unbatch() unbatches", t => {
     const expectedElements = ["a", "b", "c"];
     let i = 0;
     source
-        .pipe(batch(3, undefined, { objectMode: true }))
+        .pipe(batch(3))
         .pipe(unbatch())
         .on("data", (element: string) => {
             expect(element).to.equal(expectedElements[i]);
