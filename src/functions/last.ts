467b70c66a5fb1f470f226cb3ab6c6fd95e89c22
Lewis Diamond <git@lewisdiamond.com>
2023-02-04 10:36:05 -0500

    Fix last() not rejecting on error.
    Fixes #4

--- src/functions/last.ts
+++ src/functions/last.ts
@@ -1,8 +1,9 @@
 export function last<T>(readable: NodeJS.ReadableStream): Promise<T | null> {
     let lastChunk: T | null = null;
-    return new Promise((resolve, _) => {
+    return new Promise((resolve, reject) => {
         readable
             .on("data", (chunk) => (lastChunk = chunk))
+            .on("error", (e) => reject(e))
             .on("end", () => resolve(lastChunk));
     });
 }
--- tests/last.spec.ts
+++ tests/last.spec.ts
@@ -1,9 +1,9 @@
 import { Readable } from "stream";
 import test from "ava";
 import { expect } from "chai";
-import { last } from "../src";
+import { last, map } from "../src";
 
-test("last() resolves to the last chunk streamed by the given readable stream", async t => {
+test("last() resolves to the last chunk streamed by the given readable stream", async (t) => {
     const source = new Readable({ objectMode: true });
     const lastPromise = last(source);
     source.push("ab");
@@ -12,4 +12,18 @@ test("last() resolves to the last chunk streamed by the given readable stream", 
     source.push(null);
     const lastChunk = await lastPromise;
     expect(lastChunk).to.equal("ef");
+});
+
+test("last() rejects the promise if an error occurs in the stream", (t) => {
+    const source = new Readable({ objectMode: true, read: () => {} });
+    const out = map((x) => {
+        throw x;
+    });
+    const lastPromise = last(source.pipe(out));
+    source.push("ab");
+    lastPromise
+        .then(() => t.fail())
+        .catch((e) => {
+            expect(e).equal("ab");
+        });
 });
